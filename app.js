920d012f6cb77becb8ea91bcc60d5f082a8b2bb9
Zach Morse <zacharymorse@gmail.com>
2017-06-20 15:55:26 -0500

    added css file and fixed functionality of JS

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ const app = express();
 const data = require("./data.js");
 const port = 8000;
 
-var mustache = require("mustache-express");
+const mustacheExpress = require("mustache-express");
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", "./views");
+
+app.get("/", function(req, res) {
+  res.render("index", { userListing: data.users });
+});
+
+app.use(express.static("views"));
+
+app.listen(port, function() {
+  console.log("Server is running on port " + port);
+});
